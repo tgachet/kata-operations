ab3c0280d7d65f7f1bd9cc018d75cd076fc1ba2c
Thomas Gachet <gachet.thomas@gmail.com>
2019-10-17 16:24:06 +0200

    ref test 23

--- src/Operation.php
+++ src/Operation.php
@@ -11,9 +11,7 @@ class Operation
         array_walk($numbers, function ($number){
             return (int) $number;
         });
-//        $first = reset($numbers);
-//        $second = $numbers[1];
-//        $thrid = $numbers[2];
+
         list($first, $second, $third) = $numbers;
 
         return $first + $second + $third;
