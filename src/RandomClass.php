b0773ccccfe43254d3d35dc153bf652f91fe5298
thomasgachet <gachet.thomas@gmail.com>
2020-03-11 13:13:41 +0100

    test for sonar

---
+++ src/RandomClass.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+class RandomClass
+{
+    public function notagood_name()
+    {
+        if (true) {
+            if (false) {
+                return true;
+            }
+            return false;
+        }
+
+        return;
+    }
+}
